7e03394ca4af73df6ded098f8807eeda5b4a73dd
msalzarulo <msalzarulo@integrity-apps.com>
2019-06-07 15:57:41 -0400

    Convert shape file to raster tif image base on heuristic pixel resolution.

--- scratch_pad.py
+++ scratch_pad.py
@@ -6,6 +6,7 @@
 ## Description:
 # This script will scan the appropriate paths and pull the relevant files. It will use the files to create a binary
 # array corresponding to the mapped coordinates of the files of interest.
+# ERSI Shapefile white pages: https://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
 
 from osgeo import ogr
 import gdal, os, subprocess
@@ -23,18 +24,34 @@ for root, _, file in os.walk(shp_file_path):
 
 # This code originates from the gdal api tutorial found at: https://gdal.org/tutorials/raster_api_tut.html
 dataset = gdal.OpenEx(shape_file[0], gdal.OF_VECTOR)
-dataset_layer = dataset.Getlayer() # This line is busted
+# dataset = ogr.Open(shape_file[0]) # This is outdated
+
+dataset_layer = dataset.GetLayer()
+x_min, x_max, y_min, y_max = dataset_layer.GetExtent()  # This information may not used once reference projection is found
+
 if dataset is None:
     gdal.GetLastErrorMsg()
-Output_mask = gdal.Open(mask_file[0], gdal.GA_ReadOnly)
-Output = gdal.GetDriverByName('GTiff').Create(Output_mask, Output_mask.RasterXSize, Output_mask.RasterYSize, 1,
-                                              gdal.GDT_Byte, options=['COMPRESS=DEFLATE'])
-Output.SetProjection(Output_mask.GetProjectionRef())
-Output.SetGeoTransform(Output_mask.GetGeoTransform())
+
+# This method was found here : https://gis.stackexchange.com/questions/222394/how-to-convert-file-shp-to-tif-using-ogr-or-python-or-gdal
+# Output_mask = gdal.OpenEx(mask_file[0], gdal.OF_VECTOR)  # This line is also busted
+# Output = gdal.GetDriverByName('GTiff').Create(Output_mask, Output_mask.RasterXSize, Output_mask.RasterYSize, 1,
+#                                              gdal.GDT_Byte, options=['COMPRESS=DEFLATE'])
+# Output.SetProjection(Output_mask.GetProjectionRef())
+# Output.SetGeoTransform(Output_mask.GetGeoTransform())
+
+
+# This method was found in the ogr doumentation at : https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
+pixel_size = 25  # huristic value.... yes I made it up
+x_res = int((x_max - x_min) / pixel_size)  # set resolution on x axis
+y_res = int((y_max - y_min) / pixel_size)  # set resolution on y axis
+
+Output = gdal.GetDriverByName('GTiff').Create('ugly_mask.tif', x_res, y_res, 1, gdal.GDT_Byte) #There is an error in this line
+
+Output.SetGeoTransform((x_min,pixel_size,0,y_max,pixel_size,0))
 Band = Output.GetRasterBand(1)
 Band.SetNoDataValue(0)
-gdal.RasterizeLayer(Output, [1], dataset_layer,burn_values=[1])
-subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE "+OutputImage+" 2 4 8 16 32 64", shell=True)
+gdal.RasterizeLayer(Output, [1], dataset_layer, burn_values=[1])
+subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE " + "mask.tif" + " 2 4 8 16 32 64", shell=True)
 print("End stolen code")
 
 # # This code was taken from https://pcjericks.github.io/py-gdalogr-cookbook/layers.html
