0c20e1d9c07ab6c133df7547b13476c98ee6834b
msalzarulo <msalzarulo@integrity-apps.com>
2019-06-10 14:26:13 -0400

    added option to convert shp to tiff based on existing raster file. Removed excess code and comments.

--- scratch_pad.py
+++ scratch_pad.py
@@ -16,6 +16,7 @@ gdal.AllRegister()
 # Need to find a better method of doing this
 # Points to the directory where the mapped data of the shape files live
 shp_file_path = "C:\\Users\\msalzarulo\\Documents\\skynetV2\\Pre Cyclone Idai\\Mapped_data"
+reference_raster = "C:\\Users\\msalzarulo\\Documents\\skynetV2\\Pre Cyclone Idai\\0023231.tif"
 
 # Scans the directory and saves a list of shape file paths in shape_file
 for root, _, file in os.walk(shp_file_path):
@@ -24,62 +25,33 @@ for root, _, file in os.walk(shp_file_path):
 
 # This code originates from the gdal api tutorial found at: https://gdal.org/tutorials/raster_api_tut.html
 dataset = gdal.OpenEx(shape_file[0], gdal.OF_VECTOR)
-# dataset = ogr.Open(shape_file[0]) # This is outdated
-
-dataset_layer = dataset.GetLayer()
-x_min, x_max, y_min, y_max = dataset_layer.GetExtent()  # This information may not used once reference projection is found
-
+# check if file is open
 if dataset is None:
     gdal.GetLastErrorMsg()
-
-# This method was found here : https://gis.stackexchange.com/questions/222394/how-to-convert-file-shp-to-tif-using-ogr-or-python-or-gdal
-# Output_mask = gdal.OpenEx(mask_file[0], gdal.OF_VECTOR)  # This line is also busted
-# Output = gdal.GetDriverByName('GTiff').Create(Output_mask, Output_mask.RasterXSize, Output_mask.RasterYSize, 1,
-#                                              gdal.GDT_Byte, options=['COMPRESS=DEFLATE'])
-# Output.SetProjection(Output_mask.GetProjectionRef())
-# Output.SetGeoTransform(Output_mask.GetGeoTransform())
-
+# check if file is open
+refimage = gdal.Open(reference_raster,gdal.GA_ReadOnly)
+if refimage is None:
+    gdal.GetLastErrorMsg()
+# dataset = ogr.Open(shape_file[0]) # This is outdated
+dataset_layer = dataset.GetLayer()
+x_min, x_max, y_min, y_max = dataset_layer.GetExtent()  # This information may not used once reference projection is found
 # This method was found in the ogr doumentation at : https://pcjericks.github.io/py-gdalogr-cookbook/raster_layers.html
 pixel_size = 25  # huristic value.... yes I made it up
 x_res = int((x_max - x_min) / pixel_size)  # set resolution on x axis
+if x_res == 0:
+    x_res = 1 # Set to 1 for gdal create parameters
 y_res = int((y_max - y_min) / pixel_size)  # set resolution on y axis
-
-Output = gdal.GetDriverByName('GTiff').Create('ugly_mask.tif', x_res, y_res, 1, gdal.GDT_Byte) #There is an error in this line
-
+if y_res == 0:
+    y_res = 1 # Set to 1 for gdal create parameters
+# driver.Create(filename,xval,yval,bands,datatype)
+# Output = gdal.GetDriverByName('GTiff').Create('ugly_mask.tif', x_res, y_res, 1, gdal.GDT_Byte)
+Output = gdal.GetDriverByName('GTiff').Create('ugly_mask.tif', 1000, 1000, 1, gdal.GDT_Byte)
 Output.SetGeoTransform((x_min,pixel_size,0,y_max,pixel_size,0))
 Band = Output.GetRasterBand(1)
 Band.SetNoDataValue(0)
 gdal.RasterizeLayer(Output, [1], dataset_layer, burn_values=[1])
-subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE " + "mask.tif" + " 2 4 8 16 32 64", shell=True)
+subprocess.call("gdaladdo --config COMPRESS_OVERVIEW DEFLATE " + "ugly_mask.tif" + " 2 4 8 16 32 64", shell=True)
 print("End stolen code")
 
-# # This code was taken from https://pcjericks.github.io/py-gdalogr-cookbook/layers.html
-# # I bleieve it is outdated
-# def world2Pixel(geoMatrix, x, y):
-#   """
-#   Uses a gdal geomatrix (gdal.GetGeoTransform()) to calculate
-#   the pixel location of a geospatial coordinate
-#   """
-#   ulX = geoMatrix[0]
-#   ulY = geoMatrix[3]
-#   xDist = geoMatrix[1]
-#   yDist = geoMatrix[5]
-#   rtnX = geoMatrix[2]
-#   rtnY = geoMatrix[4]
-#   pixel = int((x - ulX) / xDist)
-#   line = int((ulY - y) / xDist)
-#   return (pixel, line)
-#
-# driver = ogr.GetDriverByName("ESRI Shapefile")
-# shapes = driver.Open(shape_file[0],0)
-# lyr = shapes.GetLayer()
-# poly=lyr.GetNextFeature()
-# geom=poly.GetGeometryRef()
-# pts = geom.GetGeometryRef(0)
-# points = []
-# pixels = []
-# for p in range(pts.GetPointCount()):
-#     points.append((pts.GetX(p),pts.GetY(p)))
-# for p in points:
-#     pixels.append(world2Pixel(geoTrans,p[0],p[1]))
-print(dataset)
+# # Note to self:
+# This code will run however the resulting tif does not display as expected
